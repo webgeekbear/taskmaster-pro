fef72e7943fff35483f321d6002207639c9711bb
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-10-12 18:53:48 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   let taskText = $("#modalTaskDescription").val();
   let taskDate = $("#modalDueDate").val();
@@ -195,16 +195,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store task data in
@@ -245,10 +247,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -266,7 +268,6 @@ $("#modalDueDate").datepicker({
 function auditTask(taskEl) {
   // Get the date from a task element
   let date = $(taskEl).find("span").text().trim();
-  console.log(date);
 
   // Convert to moment object at 5:00pm
   let time = moment(date, "L").set("hour", 17);
